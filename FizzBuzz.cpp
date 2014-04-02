d0a9020db0eab96e485e8763dde5ad143389029c
Ryan Chase <ryanfcha@usc.edu>
2014-04-02 15:54:32 -0700

    now i really merged it.

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -2,13 +2,13 @@
  
 int main(int argc, char *argv[]) {
 	for(unsigned int i = 0; i < 100; i++) {
-<<<<<<< HEAD
+
 		if( i % 3 == 0)
 			std::cout << i << ": " << "Fizz" << std::endl;
-=======
+
 		if( i % 5 == 0)
 			std::cout << i << ": " << "Buzz" << std::endl;
->>>>>>> 85f61dc77c949d481092ea65ad1588aa892adf23
+
 	}
 	
 	return(0);
